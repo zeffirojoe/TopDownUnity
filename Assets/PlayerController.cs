ba96cf12a14cfee710d432158aa0a246e3826cf8
Joe Zeffiro <joezeffiro@gmail.com>
2022-05-31 22:38:24 -0400

    First Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementInput;
+    Vector2 mousePos;
+    Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+            }
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(0, movementInput.y));
+            }
+
+            animator.SetBool("isMoving", success);
+
+            //Mouse for aiming
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        // Sprite direction set to movement direction
+        if (movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movementInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (mousePos != Vector2.zero)
+        {
+            Vector2 lookDir = mousePos - rb.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else { return false; }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject axePrefab;
+
+    public float bulletForce = 4f;
+
+    void OnFire()
+    {
+        Debug.Log("Fire");
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //if (mousePos != Vector2.zero)
+        //{
+        //    Vector2 lookDir = mousePos - rb.position;
+        //    float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        //}
+        GameObject axe = Instantiate(axePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = axe.GetComponent<Rigidbody2D>();
+        float direction = Random.Range(0, 10) < 5 ? -1.0f : 1.0f;
+
+        Vector3 tmp = new Vector3(0.35f * direction, 1.0f, 0.0f);
+        rb.AddForce(tmp * bulletForce, ForceMode2D.Impulse);
+    }
+
+}
